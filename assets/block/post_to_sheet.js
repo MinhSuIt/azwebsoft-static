fb33a70612582fca658376b8cf27e1e4e2cca6e6
minhsudoit <minhsudoit@gmail.com>
2026-05-26 22:05:54 +0700

    s

--- assets/block/post_to_sheet.js
+++ assets/block/post_to_sheet.js
@@ -1,7 +1,11 @@
 const ask_form = document
     .getElementById('ask_form')
+let url = window.askUrl
+const debug = window.debug
+if (debug) {
+    url = 'https://script.google.com/macros/s/AKfycbwy85ZcpBMf8eu8m80Lwl9cjxdTU7zh01XRRvGRFkp-FjmPFQ4hdofwoKQPL49Uguz7/exec'
+}
 ask_form.addEventListener('submit', async function (e) {
-
     e.preventDefault();
 
     const data = {
@@ -9,16 +13,47 @@ ask_form.addEventListener('submit', async function (e) {
         email: this.email.value,
         message: this.message.value
     };
-    await fetch(
-        'https://script.google.com/macros/s/AKfycbwy85ZcpBMf8eu8m80Lwl9cjxdTU7zh01XRRvGRFkp-FjmPFQ4hdofwoKQPL49Uguz7/exec', {
+    const token = this._token.value;
+    let params = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
         mode: 'no-cors',
         body: JSON.stringify(data)
     }
-    );
-    ask_form.reset();
-    alert('Cảm ơn đã đóng góp, chúng tôi sẽ liên hệ với bạn sớm nhất có thể!');
+    if (debug) {
+        params.headers['X-CSRF-TOKEN'] = token
+        params.headers['Accept'] = 'application/json'
+        params.credentials = 'same-origin'
+        params.mode = 'cors'
+    }
+    try {
+
+        const response = await fetch(url, params);
+        if (debug) {
+            const result = await response.json();
+
+            if (!response.ok) {
+
+                let messages = [];
+
+                Object.values(result.errors).forEach(error => {
+                    messages.push(error[0]);
+                });
+
+                alert(messages.join('\n'));
+
+                return;
+            }
+        }
+
+        ask_form.reset();
+        alert('Cảm ơn đã đóng góp, chúng tôi sẽ liên hệ với bạn sớm nhất có thể!');
+
+    } catch (error) {
+
+        alert('Có lỗi xảy ra!');
+
+    }
 });
